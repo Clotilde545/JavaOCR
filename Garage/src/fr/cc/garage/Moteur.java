c9542298b624b0328f4ea864f4f19968bd78caa9
Clotilde <clotildemangeol@gmail.com>
2019-06-11 14:01:52 +0200

    création de la classe moteur

--- Garage/src/fr/cc/garage/Moteur.java
+++ Garage/src/fr/cc/garage/Moteur.java
@@ -1,4 +1,37 @@
 package fr.cc.garage;
 
 public class Moteur {
+    protected TypeMoteur type;
+    protected String cylindre;
+    protected double prixMoteur;
+
+    public void  Moteur(String cylindre, double prixMoteur){
+        this.cylindre = cylindre;
+        this.prixMoteur=prixMoteur;
+
+    }
+
+    public TypeMoteur getType() {
+        return type;
+    }
+
+    public void setType(TypeMoteur type) {
+        this.type = type;
+    }
+
+    public String getCylindre() {
+        return cylindre;
+    }
+
+    public void setCylindre(String cylindre) {
+        this.cylindre = cylindre;
+    }
+
+    public double getPrixMoteur() {
+        return prixMoteur;
+    }
+
+    public void setPrixMoteur(double prixMoteur) {
+        this.prixMoteur = prixMoteur;
+    }
 }
