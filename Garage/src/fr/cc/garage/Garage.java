5c272e55beeed34efba819137570e9c95a05c0e7
Clotilde <clotildemangeol@gmail.com>
2019-06-11 11:46:37 +0200

    création de la classe garage

--- Garage/src/fr/cc/garage/Garage.java
+++ Garage/src/fr/cc/garage/Garage.java
@@ -1,4 +1,49 @@
 package fr.cc.garage;
 
+import java.util.List;
+
 public class Garage extends Vehicule {
+    protected double prixVehicule;
+    protected String nomVehicule;
+    protected List listVehicule = new List<Options>;
+    protected String nomMarque;
+
+    public List addOptions(String opt){
+       listVehicule.add(opt);
+       return listVehicule;
+    }
+
+    public double getPrixVehicule() {
+        return prixVehicule;
+    }
+
+    public void setPrixVehicule(double prixVehicule) {
+        this.prixVehicule = prixVehicule;
+    }
+
+    public String getNomVehicule() {
+        return nomVehicule;
+    }
+
+    public void setNomVehicule(String nomVehicule) {
+        this.nomVehicule = nomVehicule;
+    }
+
+    public String getNomMarque() {
+        return nomMarque;
+    }
+
+    public void setNomMarque(String nomMarque) {
+        this.nomMarque = nomMarque;
+    }
+
+    @Override
+    public String toString() {
+        return "Garage{" +
+                "prixVehicule=" + prixVehicule +
+                ", nomVehicule='" + nomVehicule + '\'' +
+                ", listVehicule=" + listVehicule +
+                ", nomMarque='" + nomMarque + '\'' +
+                '}';
+    }
 }
