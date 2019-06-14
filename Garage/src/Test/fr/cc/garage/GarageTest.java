0a0a77da9817fedfedb3bf25450efe212ef133ac
Clotilde <clotildemangeol@gmail.com>
2019-06-14 11:26:52 +0200

    commit avant merge

--- Garage/src/Test/fr/cc/garage/GarageTest.java
+++ Garage/src/Test/fr/cc/garage/GarageTest.java
@@ -1,17 +1,41 @@
 package fr.cc.garage;
 
+import fr.cc.Main;
 import fr.cc.garage.Garage;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
+import org.junit.jupiter.api.*;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+
 public class GarageTest {
+    Main main = new Main();
+    Garage garage = new Garage();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
@@ -21,10 +45,14 @@ public class GarageTest {
 
     @org.junit.Test
     public void addVoiture() {
+
     }
 
     @org.junit.Test
     public void getPrixVehicule() {
+        garage.setPrixVehicule(5000);
+        assertEquals(5000, outContent.toString().replace("\r\n", "\n"));
+
     }
 
     @org.junit.Test
